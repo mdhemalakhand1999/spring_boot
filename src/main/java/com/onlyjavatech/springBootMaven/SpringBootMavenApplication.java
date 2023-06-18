d8c958a186a44d534b4aacdacce334d6d5f01867
hemalrika <hemalrika@gmail.com>
2023-06-18 21:45:23 +0600

    first commit

---
+++ src/main/java/com/onlyjavatech/springBootMaven/SpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.onlyjavatech.springBootMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+
+}
